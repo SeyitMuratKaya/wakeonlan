c2743f90c1bedeaec6fe80983cf27ec18164608c
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2023-03-17 18:22:53 +0300

    add dialog form validation

--- lib/open_dialog.dart
+++ lib/open_dialog.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 
 Future<List<String>?> openDialog(
-        context, message, nameController, ipController, macController) =>
-    showDialog<List<String>>(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => AlertDialog(
-        title: Text(message),
-        content: SingleChildScrollView(
+    context, message, nameController, ipController, macController) {
+  final formKey = GlobalKey<FormState>();
+
+  return showDialog<List<String>>(
+    context: context,
+    barrierDismissible: false,
+    builder: (context) => AlertDialog(
+      title: Text(message),
+      content: SingleChildScrollView(
+        child: Form(
+          key: formKey,
           child: ListBody(
             children: [
-              TextField(
+              TextFormField(
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Name field can not be empty";
+                  }
+                  return null;
+                },
                 controller: nameController,
                 decoration: const InputDecoration(
                   hintText: "Device Name",
                   labelText: "Device Name",
                 ),
               ),
-              TextField(
+              TextFormField(
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Ip Address field can not be empty";
+                  }
+                  return null;
+                },
                 controller: ipController,
                 decoration: const InputDecoration(
                   hintText: "Ip Address",
                   labelText: "Ip Address",
                 ),
               ),
-              TextField(
+              TextFormField(
+                validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return "Mac Address field can not be empty";
+                  }
+                  return null;
+                },
                 controller: macController,
                 decoration: const InputDecoration(
                   hintText: "Mac Address",
@@ -34,24 +56,28 @@ Future<List<String>?> openDialog(
             ],
           ),
         ),
-        actions: [
-          TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text("Cancel")),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop([
-                  nameController.text.toString(),
-                  ipController.text.toString(),
-                  macController.text.toString()
-                ]);
-                nameController.clear();
-                ipController.clear();
-                macController.clear();
-              },
-              child: const Text("Save")),
-        ],
       ),
-    );
+      actions: [
+        TextButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text("Cancel")),
+        ElevatedButton(
+            onPressed: () {
+              if(formKey.currentState!.validate()){
+                Navigator.of(context).pop([
+                nameController.text.toString(),
+                ipController.text.toString(),
+                macController.text.toString()
+              ]);
+              nameController.clear();
+              ipController.clear();
+              macController.clear();
+              }
+            },
+            child: const Text("Save")),
+      ],
+    ),
+  );
+}
