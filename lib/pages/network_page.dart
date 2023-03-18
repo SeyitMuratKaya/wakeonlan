8db23d062f4f5e669070bb38579533d1473d0030
Seyit Murat Kaya <murat.kaya1269@gmail.com>
2023-03-18 22:37:10 +0300

    network page ui update

--- lib/pages/network_page.dart
+++ lib/pages/network_page.dart
@@ -3,6 +3,8 @@ import 'package:lan_scanner/lan_scanner.dart';
 import '../models/models.dart';
 import '../network_item.dart';
 
+final List<Item> lanComputers = <Item>[];
+
 class NetworkPage extends StatefulWidget {
   const NetworkPage({super.key});
 
@@ -11,25 +13,28 @@ class NetworkPage extends StatefulWidget {
 }
 
 class _NetworkPageState extends State<NetworkPage> {
-  final List<Item> lanComputers = <Item>[];
   double progressIndicator = 0.0;
+  bool scanState = false;
 
   @override
   void initState() {
-    setState(() {
-      lanComputers.clear();
-    });
-    scan();
     super.initState();
   }
 
   Future<void> scan() async {
+    setState(() {
+      lanComputers.clear();
+      scanState = true;
+    });
+
     final scanner = LanScanner();
 
     final stream = scanner.icmpScan('192.168.1', progressCallback: (progress) {
-      // print('Progress: $progress');
       setState(() {
         progressIndicator = progress;
+        if (progress == 1.0) {
+          scanState = false;
+        }
       });
     });
 
@@ -48,24 +53,43 @@ class _NetworkPageState extends State<NetworkPage> {
         title: const Text("Network"),
         bottom: PreferredSize(
           preferredSize: const Size(double.infinity, 1.0),
-          child: LinearProgressIndicator(value: progressIndicator),
+          child: Visibility(
+              visible: scanState,
+              child: LinearProgressIndicator(value: progressIndicator)),
         ),
         actions: [
           IconButton(
             icon: const Icon(Icons.refresh),
             tooltip: 'Refresh Devices',
             onPressed: () {
-              //search network
+              if (!scanState) {
+                scan();
+              }
             },
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: lanComputers.length,
-        itemBuilder: (context, index) {
-          return NetworkItem(item: lanComputers[index]);
-        },
-      ),
+      body: lanComputers.isEmpty
+          ? Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.height / 4,
+                height: MediaQuery.of(context).size.width / 4,
+                child: ElevatedButton(
+                    onPressed: () {
+                      scan();
+                    },
+                    child: const Text(
+                      "Scan",
+                      textScaleFactor: 2,
+                    )),
+              ),
+            )
+          : ListView.builder(
+              itemCount: lanComputers.length,
+              itemBuilder: (context, index) {
+                return NetworkItem(item: lanComputers[index]);
+              },
+            ),
     );
   }
 }
